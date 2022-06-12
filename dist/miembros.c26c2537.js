0f9d606ada80e605bc589e75f152f532a542514f
Ana Sancho Callealta <asanchoca@uoc.edu>
2022-06-12 18:15:08 +0200

    pec3

---
+++ dist/miembros.c26c2537.js
@@ -0,0 +1,8 @@
+function mostrarCuriosidadMiembros() {
+    document.getElementById("curiosidad").innerHTML = "Los RHCP han ganado siete Premios Grammy. En 2011 fueron elegidos para ingresar en el Salón de la Fama del Rock and Roll.";
+}
+function mostrarCuriosidadHistory() {
+    document.getElementById("curiosidad").innerHTML = "En sus primeras actuaciones, los componentes del grupo a veces actuaban prácticamente desnudos, con solo un calcetín en los genitales.";
+}
+
+//# sourceMappingURL=miembros.c26c2537.js.map
